9ff96f88628054d48690d7348a8f65f06405e155
AbrahamK18 <kpatakouabraham6@gmail.com>
2026-08-11 07:29:16 +0100

    Initialize Supabase client in config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://iywcumouaklfkrjdiehj.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_8gIwisPdOp-T7uPSGHbyQQ_lKwrAa4s";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
